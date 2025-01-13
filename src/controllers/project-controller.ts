4f8af48041a816e4ee04c080095fbddf12866415
tejasmahajan2 <tejas.mahajan@in.spundan.com>
2025-01-13 16:41:06 +0530

    removed unused utitlites

--- src/controllers/project-controller.ts
+++ src/controllers/project-controller.ts
@@ -46,7 +46,7 @@ export async function validateProject(req: Request, res: Response, next: NextFun
 export async function validateModule(req: Request, res: Response, next: NextFunction) {
     const { resourceName, projectName, id } = getResourceComponents(req);
     const { projectEntity } = req.body;
- 
+
     const moduleFound = await modulesCollection.findOne({
         name: resourceName,
         projectId: `${projectEntity._id}`
@@ -96,8 +96,12 @@ export async function validateParams(req: Request, res: Response, next: NextFunc
         } else {
             // but if method is get with url params then get specific resource
             if (req.method === "GET") {
-                res.send(await model.findOne({_id : id}))
-            } else {
+                res.send(await model.findOne({ _id: id }))
+            } else if (req.method === "DELETE") {
+                await model.deleteOne({ _id: id })
+                res.send("Obj deleted successfully.");
+            }
+            else {
                 const validate = ajv.compile(resourceEntity.schema);
                 if (!validate(body)) {
                     res.status(400).json({ errors: validate.errors });
@@ -118,7 +122,7 @@ export async function validatePayload(req: Request, res: Response, next: NextFun
     const { resourceName, projectName, id } = getResourceComponents(req);
     const { projectEntity, moduleEntity, resourceEntity, ...body } = req.body;
 
-    
+
     const modelName = `${projectName}_${resourceName}`.toLowerCase();
     const model = getOrCreateModel(modelName);
 
@@ -154,81 +158,126 @@ export async function processRequest(req: Request, res: Response, next: NextFunc
     }
 }
 
-// export async function validateSchema(req: Request, res: Response, next: NextFunction) {
+// async function validateRequest(req: Request, res: Response, next: NextFunction) {
 //     const { projectName } = req.params;
-//     const { typeEntity, projectEntity, ...body } = req.body;
-
-//     const resourceComponents = req.originalUrl.replace(`/${projectName}/`, "").split('/').filter(endpoints => endpoints !== '');
-
-//     console.log(resourceComponents)
-//     const resource = resourceComponents[0];
-//     const id = resourceComponents[1];
-//     console.log(resource)
-
-//     const schemaEntity = await schemaEntitiesCollection.findOne({
-//         method: req.method.toLocaleLowerCase(),
-//         resource,
-//         uuid: typeEntity?.schemaId,
-//         isDeleted: false,
+//     const projectFound = await projectsCollection.findOne({
+//         name: projectName,
+//         isDeleted: false
 //     });
 
-//     if (!schemaEntity) {
+//     if (!projectFound) {
 //         res.status(404).send(`Cannot ${req.method} /${projectName}`);
 //     } else {
-//         console.log(schemaEntity);
-//         // Check if the collection exists
-//         const collectionName = `${projectName}_${resource}`.toLocaleLowerCase();
+//         const typeFound = await typesCollection.findOne({
+//             projectId: projectFound?.uuid,
+//             organizationId: projectFound?.organizationId,
+//             isDeleted: false
+//         });
 
-//         // Create a generic schema and models
-//         let DynamicModel = getOrCreateModel(collectionName)
-
-//         const validationMethods = ["POST", "PUT", "PATCH"];
-
-//         if (validationMethods.includes(req.method)) {
-//             const validate = ajv.compile(schemaEntity.schema);
-//             console.log(body)
-//             if (!validate(body)) {
-//                 // const errors = validate.errors?.map((e) => `${e?.message} "${e?.params?.additionalProperty}"`);
-//                 res.status(400).json({ errors: validate.errors });
-//             } else {
-//                 switch (req?.method) {
-//                     case "POST":
-//                         res.send(await DynamicModel.create(body));
-//                         break;
-//                     case "PUT":
-//                         if (id) {
-//                             if (!mongoose.Types.ObjectId.isValid(id)) {
-//                                 res.status(400).json({ error: 'Invalid ObjectId' });
-//                             } else {
-//                                 res.send(await DynamicModel.findOne({ _id: id }));
-//                             }
-//                         } else {
-//                             res.status(404).send(`Cannot ${req.method} /${projectName}`);
-//                         }
-//                         break;
-//                     default:
-//                         break;
-//                 }
-//             }
+//         if (!typeFound) {
+//             res.status(404).send(`Cannot ${req.method} /${projectName}`);
 //         } else {
-//             switch (req?.method) {
-//                 case "GET":
-//                     if (id) {
-//                         if (!mongoose.Types.ObjectId.isValid(id)) {
-//                             res.status(400).json({ error: 'Invalid ObjectId' });
-//                         } else {
-//                             res.send(await DynamicModel.findOne({ _id: id }));
-//                         }
-//                     } else {
-//                         res.send(await DynamicModel.find());
-//                     }
-//                     break;
-//                 case "POST":
-//                 default:
-//                     break;
-//             }
-//         }
+//             // If methods is not POST or PUT we dont need to validate schema
+//             const resource = req.originalUrl.split(`/${projectName}/`)[1];
+//             console.log(resource);
 
-//         next();
+//             const schemaEntity = await schemaEntitiesCollection.findOne({
+//                 method: req.method.toLocaleLowerCase(),
+//                 resource,
+//                 uuid: typeFound?.schemaId,
+//                 isDeleted: false,
+//             });
+
+//             if (!schemaEntity) {
+//                 res.status(404).send(`Cannot ${req.method} ${req.originalUrl}`);
+//             } else {
+//                 // Check if the collection exists
+//                 const collectionName = `${projectName}_${resource}`.toLocaleLowerCase();
+
+//                 // Create a generic schema and models
+//                 let DynamicModel = mongoose.models[collectionName];
+//                 if (!DynamicModel) {
+//                     const dynamicSchema = new mongoose.Schema(BaseSchemaFields, { strict: false });
+//                     DynamicModel = mongoose.model(collectionName, dynamicSchema, collectionName);
+//                 }
+
+//                 const validationMethods = ["POST", "PUT", "PATCH"];
+
+//                 if (validationMethods.includes(req.method)) {
+//                     const validate = ajv.compile(schemaEntity.schema);
+//                     if (!validate(req.body)) {
+//                         // const errors = validate.errors?.map((e) => `${e?.message} "${e?.params?.additionalProperty}"`);
+//                         res.status(400).json({ errors: validate.errors });
+//                     } else {
+//                         switch (req?.method) {
+//                             case "POST":
+//                                 res.send(await DynamicModel.create(req.body));
+//                                 break;
+//                             default:
+//                                 break;
+//                         }
+//                     }
+//                 } else {
+//                     switch (req?.method) {
+//                         case "GET":
+//                             res.send(await DynamicModel.find());
+//                             break;
+//                         case "POST":
+//                         default:
+//                             break;
+//                     }
+//                 }
+
+//             }
+
+
+//             // if (!schemaEntity) {
+//             //     res.status(404).send(`Cannot ${req.method} ${req.originalUrl}`);
+//             // } else {
+//             //     const validate = ajv.compile(schemaEntity.schema);
+//             //     if (!validate(req.body)) {
+//             //         const errors = validate.errors?.map((e) => `${e?.message} "${e?.params?.additionalProperty}"`);
+//             //         res.status(400).json({ errors });
+//             //     } else {
+
+//             //         // Check if the collection exists
+//             //         const collectionName = `${projectName}_${resource}`.toLocaleLowerCase();
+
+//             //         let DynamicModel = mongoose.models[collectionName];
+//             //         if (!DynamicModel) {
+//             //             // Create a generic schema
+//             //             const dynamicSchema = new mongoose.Schema(BaseSchemaFields, { strict: false });
+//             //             DynamicModel = mongoose.model(collectionName, dynamicSchema, collectionName);
+//             //         }
+
+//             //         switch (req?.method) {
+//             //             case "GET":
+//             //                 res.send(await DynamicModel.find());
+//             //                 break;
+//             //             case "POST":
+//             //                 res.send(await DynamicModel.create(req.body));
+//             //                 break;
+//             //             default:
+//             //                 break;
+//             //         }
+
+//             //         // const logicEntity = await bussinessLogicsCollection.findOne({ schemaId: schemaEntity?.uuid, isDeleted: false, });
+
+//             //         // if (!logicEntity) {
+//             //         //     res.status(404).send(`Cannot ${req.method} ${req.originalUrl}`);
+//             //         // } else {
+//             //         //     try {
+//             //         //         next();
+//             //         //         const evalutedFunc = eval(logicEntity.logic)(req, res, next);
+//             //         //         if (!evalutedFunc) next();
+//             //         //         else app.use('/' + projectName, evalutedFunc);
+//             //         //     } catch (error) {
+//             //         //         console.log(error);
+//             //         //         res.status(500).send(`Unexpect error occured!`);
+//             //         //     }
+//             //         // }
+//             //     }
+//             // }
+//         }
 //     }
 // }
--- src/index.ts
+++ src/index.ts
@@ -1,166 +1,27 @@
 import dotenv from "dotenv";
 dotenv.config({});
 
-import express, { Express, NextFunction, Request, Response } from "express";
+import express, { Express, Request, Response } from "express";
 import cors from "cors";
 import connectDB from "./config/database";
-import mongoose from "mongoose";
-import Ajv from "ajv";
-import addFormats from "ajv-formats";
 import swaggerRouter from "./routes/swagger-router";
 import projectRouter from "./routes/project-router";
-import { BaseSchemaFields } from "./schemas/base-schema";
 
-const ajv = new Ajv();
-addFormats(ajv);
 
 const app: Express = express();
-connectDB();
-const port = process.env.PORT || 3000;
+
 app.use(cors());
 app.use(express.json());
 app.use('/', swaggerRouter);
 app.use('/', projectRouter);
 
-const projectsCollection = mongoose.connection.collection("projects");
-const typesCollection = mongoose.connection.collection("types");
-const schemaEntitiesCollection = mongoose.connection.collection("schemaentities");
-const bussinessLogicsCollection = mongoose.connection.collection("bussinesslogics");
+connectDB();
 
 app.get("/", (req: Request, res: Response) => {
     res.send("Express + TypeScript Server");
 });
 
-async function validateRequest(req: Request, res: Response, next: NextFunction) {
-    const { projectName } = req.params;
-    const projectFound = await projectsCollection.findOne({
-        name: projectName,
-        isDeleted: false
-    });
-
-    if (!projectFound) {
-        res.status(404).send(`Cannot ${req.method} /${projectName}`);
-    } else {
-        const typeFound = await typesCollection.findOne({
-            projectId: projectFound?.uuid,
-            organizationId: projectFound?.organizationId,
-            isDeleted: false
-        });
-
-        if (!typeFound) {
-            res.status(404).send(`Cannot ${req.method} /${projectName}`);
-        } else {
-            // If methods is not POST or PUT we dont need to validate schema
-            const resource = req.originalUrl.split(`/${projectName}/`)[1];
-            console.log(resource);
-
-            const schemaEntity = await schemaEntitiesCollection.findOne({
-                method: req.method.toLocaleLowerCase(),
-                resource,
-                uuid: typeFound?.schemaId,
-                isDeleted: false,
-            });
-
-            if (!schemaEntity) {
-                res.status(404).send(`Cannot ${req.method} ${req.originalUrl}`);
-            } else {
-                // Check if the collection exists
-                const collectionName = `${projectName}_${resource}`.toLocaleLowerCase();
-
-                // Create a generic schema and models
-                let DynamicModel = mongoose.models[collectionName];
-                if (!DynamicModel) {
-                    const dynamicSchema = new mongoose.Schema(BaseSchemaFields, { strict: false });
-                    DynamicModel = mongoose.model(collectionName, dynamicSchema, collectionName);
-                }
-
-                const validationMethods = ["POST", "PUT", "PATCH"];
-
-                if (validationMethods.includes(req.method)) {
-                    const validate = ajv.compile(schemaEntity.schema);
-                    if (!validate(req.body)) {
-                        // const errors = validate.errors?.map((e) => `${e?.message} "${e?.params?.additionalProperty}"`);
-                        res.status(400).json({ errors: validate.errors });
-                    } else {
-                        switch (req?.method) {
-                            case "POST":
-                                res.send(await DynamicModel.create(req.body));
-                                break;
-                            default:
-                                break;
-                        }
-                    }
-                } else {
-                    switch (req?.method) {
-                        case "GET":
-                            res.send(await DynamicModel.find());
-                            break;
-                        case "POST":
-                        default:
-                            break;
-                    }
-                }
-
-            }
-
-
-            // if (!schemaEntity) {
-            //     res.status(404).send(`Cannot ${req.method} ${req.originalUrl}`);
-            // } else {
-            //     const validate = ajv.compile(schemaEntity.schema);
-            //     if (!validate(req.body)) {
-            //         const errors = validate.errors?.map((e) => `${e?.message} "${e?.params?.additionalProperty}"`);
-            //         res.status(400).json({ errors });
-            //     } else {
-
-            //         // Check if the collection exists
-            //         const collectionName = `${projectName}_${resource}`.toLocaleLowerCase();
-
-            //         let DynamicModel = mongoose.models[collectionName];
-            //         if (!DynamicModel) {
-            //             // Create a generic schema
-            //             const dynamicSchema = new mongoose.Schema(BaseSchemaFields, { strict: false });
-            //             DynamicModel = mongoose.model(collectionName, dynamicSchema, collectionName);
-            //         }
-
-            //         switch (req?.method) {
-            //             case "GET":
-            //                 res.send(await DynamicModel.find());
-            //                 break;
-            //             case "POST":
-            //                 res.send(await DynamicModel.create(req.body));
-            //                 break;
-            //             default:
-            //                 break;
-            //         }
-
-            //         // const logicEntity = await bussinessLogicsCollection.findOne({ schemaId: schemaEntity?.uuid, isDeleted: false, });
-
-            //         // if (!logicEntity) {
-            //         //     res.status(404).send(`Cannot ${req.method} ${req.originalUrl}`);
-            //         // } else {
-            //         //     try {
-            //         //         next();
-            //         //         const evalutedFunc = eval(logicEntity.logic)(req, res, next);
-            //         //         if (!evalutedFunc) next();
-            //         //         else app.use('/' + projectName, evalutedFunc);
-            //         //     } catch (error) {
-            //         //         console.log(error);
-            //         //         res.status(500).send(`Unexpect error occured!`);
-            //         //     }
-            //         // }
-            //     }
-            // }
-        }
-    }
-}
-
-// app.use('/:projectName', validateRequest);
-
-// app.use('/:projectName/*', async (req: Request, res: Response, next: NextFunction) => {
-//     res.send(`Not found ${req.originalUrl}`);
-// });
-
+const port = process.env.PORT || 3000;
 app.listen(port, () => {
     console.log(`[server]: Server is running at http://localhost:${port}`);
 });
